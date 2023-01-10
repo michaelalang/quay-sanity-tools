3a9f00779b5d366ea066e9de62492fce05586df0
Michaela Lang <milang@redhat.com>
2023-01-10 12:29:45 +0100

    * udpate UBI to v9 * changed psycopg to psycopg2 to utilize pools * added threading for handling the per object actions (s3,db)

--- quay-db-2-storage-consistency.py
+++ quay-db-2-storage-consistency.py
@@ -1,9 +1,19 @@
 #!/opt/app-root/bin/python3
 
-import psycopg
+import psycopg2
+from psycopg2 import pool
 import boto3, botocore
 from botocore.client import Config
 import os, sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
+import optparse
+from queue import Queue
+from time import sleep
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
 
 S3CONFIG = Config(connect_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
                   read_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
@@ -12,37 +22,74 @@ S3CONFIG = Config(connect_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
 BUCKET = os.environ.get('BUCKET', '')
 REGPATH = os.environ.get('REGPATH', 'datastorage/registry/sha256')
 POSTGRESURI = os.environ.get('POSTGRESURI', False)
-try:
-    S3 = boto3.client('s3',
-                aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-                endpoint_url=os.environ.get('ENDPOINT'),
-                config=S3CONFIG,
-                verify=bool(os.environ.get('SSL_VERIFY', True)))
-except Exception as e:
-    print(str(e))
-    sys.exit(1)
+MAXTHREADS = int(os.environ.get('MAXTHREADS', 10))
+Blobs = Queue()
+Images = Queue()
 
-def resolve_image_namefrom_blob(uuid):
-    cur.execute(f"""
-    SELECT repository.name FROM imagestorage 
-    LEFT JOIN manifestblob ON imagestorage.id = manifestblob.blob_id
-    LEFT JOIN repository ON manifestblob.repository_id = repository.id
-    WHERE imagestorage.uuid = '{uuid}'
-    """)
-    return cur.fetchone()
+class S3check(object):
+    def __init__(self, bucket=None, s3config=None, parent=None):
+        self._bucket = bucket
+        self._parent = parent
+        self._s3 = boto3.client('s3',
+                    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+                    endpoint_url=os.environ.get('ENDPOINT'),
+                    config=s3config,
+                    verify=bool(os.environ.get('SSL_VERIFY', True)))
+    def check_blob_on_storage(self, record=None):
+        bdir = record[-1].split(':')[1][:2]
+        blob = record[-1].split(':')[1]
+        try:
+            logger.debug(f"calling S3 stat on {REGPATH}/{bdir}/{blob}")
+            self._s3.head_object(
+                    Bucket=self._bucket, Key=f"{REGPATH}/{bdir}/{blob}")
+        except botocore.exceptions.ClientError:
+            logger.error(f"{self._parent.resolve_image_namefrom_blob(record[0])[0]} missing blob {REGPATH}/{bdir}/{blob}")
+            return False
+        return True
 
-def check_blob_on_storage(record):
-    bdir = record[-1].split(':')[1][:2]
-    blob = record[-1].split(':')[1]
-    try:
-        pinfo = S3.head_object(
-                Bucket=BUCKET, Key=f"{REGPATH}/{bdir}/{blob}")
-    except botocore.exceptions.ClientError:
-        print(f"{resolve_image_namefrom_blob(record[0])} missing blob {REGPATH}/{bdir}/{blob}")
+class DBcheck(object):
+    def __init__(self, conn=None,):
+        self._conn = conn
+        self._cur = conn.cursor()
+        self._s3  = S3check(bucket=BUCKET, s3config=S3CONFIG, parent=self)
+    def start(self):
+        global Images, Blobs
+        while not Blobs.empty():
+            record = Blobs.get(timeout=1)
+            logger.debug(f"checking {record[0]} from DB")
+            if not self._s3.check_blob_on_storage(record):
+                Images.put(self.resolve_image_namefrom_blob(record[0]))
+    def resolve_image_namefrom_blob(self, uuid):
+        self._cur.execute(f"""
+        SELECT repository.name FROM imagestorage 
+        LEFT JOIN manifestblob ON imagestorage.id = manifestblob.blob_id
+        LEFT JOIN repository ON manifestblob.repository_id = repository.id
+        WHERE imagestorage.uuid = '{uuid}'
+        """)
+        return self._cur.fetchone()
 
-with psycopg.connect(POSTGRESURI) as dbc:
-    with dbc.cursor() as cur:
-        cur.execute("SELECT uuid, image_size, content_checksum FROM imagestorage")
-        for record in cur:
-            check_blob_on_storage(record)
+threads = []
+pgpool = psycopg2.pool.ThreadedConnectionPool(1, MAXTHREADS+1, POSTGRESURI)
+
+def fetch_db_items():
+    global Blobs, pgpool
+    with pgpool.getconn() as dbc:
+        with dbc.cursor() as cur:
+            cur.execute("SELECT count(uuid) FROM imagestorage")
+            total = cur.fetchone()[0]
+            logger.info(f"Found {total} blobs in DB")
+            cur.execute("SELECT uuid, image_size, content_checksum FROM imagestorage")
+            for record in cur:
+                Blobs.put(record)
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--debug', action='store_true', default=False)
+    options, remainings = parser.parse_args()
+    
+    if options.debug:   logger.setLevel(logging.DEBUG)
+    
+    threads.append(ThreadPoolExecutor().submit(fetch_db_items))    
+    for x in range(0, MAXTHREADS):
+        threads.append(ThreadPoolExecutor().submit(DBcheck(conn=pgpool.getconn()).start))
--- quay-storage-polution.py
+++ quay-storage-polution.py
@@ -1,9 +1,19 @@
 #!/opt/app-root/bin/python3
 
-import psycopg
+import psycopg2
+from psycopg2 import pool
 import boto3, botocore
 from botocore.client import Config
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
 import os, sys
+from time import sleep
+import logging
+import optparse
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.INFO)
 
 S3CONFIG = Config(connect_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
                   read_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
@@ -12,28 +22,75 @@ S3CONFIG = Config(connect_timeout=int(os.environ.get('BOTO3_TIMEOUT', 30)),
 BUCKET = os.environ.get('BUCKET', '')
 REGPATH = os.environ.get('REGPATH', 'datastorage/registry/sha256')
 POSTGRESURI = os.environ.get('POSTGRESURI', False)
-try:
-    S3 = boto3.client('s3',
-                aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-                endpoint_url=os.environ.get('ENDPOINT'),
-                config=S3CONFIG,
-                verify=bool(os.environ.get('SSL_VERIFY', True)))
-except Exception as e:
-    print(str(e))
-    sys.exit(1)
+DELETE = bool(os.environ.get('DELETE_I_KNOW_WHAT_I_AM_DOING', False))
+MAXTHREADS = int(os.environ.get('MAXTHREADS', 10))
 
-def check_object_in_db(cur, key):
-    cur.execute(f"SELECT uuid, content_checksum FROM imagestorage WHERE content_checksum = 'sha256:{key}' LIMIT 1")
-    if cur.fetchone() == None:    return False
-    return True
+polution = Queue()
+poluted = Queue()
+polution_empty = False
+threads = []
+total = 0
+
+pgpool = psycopg2.pool.ThreadedConnectionPool(1, MAXTHREADS+1, POSTGRESURI)
+
+class PGConnection(object):
+    def __init__(self):
+        global polution, polution_empty, pgpool
+        conn = pgpool.getconn()
+        cur = conn.cursor()
+        while not all([polution.empty(), polution_empty]):
+            blob = polution.get(timeout=1)
+            blobn = os.path.basename(blob)
+            logger.debug(f'checking Blob {blob}')
+            if not self.check_object_in_db(blobn, cur):
+                logger.debug('QuayBlob missing')
+                poluted.put(blob)
+            polution.task_done()
+            logger.debug(f'task_done iterating')
+    def check_object_in_db(self, blob, cur):
+        logger.debug(f"checking sha256:{blob}")
+        cur.execute(f"SELECT uuid, content_checksum FROM imagestorage WHERE content_checksum = 'sha256:{blob}' LIMIT 1")
+        if cur.fetchone() == None:    
+            logger.info(f"missed {blob} in DB")
+            return False
+        logger.debug(f"found {blob} in DB")
+        return True
+
+def s3thread():
+    global polution, polution_empty, threads, total
+    try:
+        S3 = boto3.client('s3',
+                    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+                    endpoint_url=os.environ.get('ENDPOINT'),
+                    config=S3CONFIG,
+                    verify=bool(os.environ.get('SSL_VERIFY', True)))
+    except Exception as e:
+        logger.error(str(e))
+        sys.exit(1)
+    paginator = S3.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=BUCKET, Prefix=REGPATH)
+    for objects in pages:
+        for info in objects.get('Contents', []):
+            logger.debug(f"adding content {info.get('Key')}")
+            polution.put(info.get('Key'))
+            total += 1
+    polution_empty = True
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--debug', action='store_true', default=False)
+    options, remainings = parser.parse_args()
     
-paginator = S3.get_paginator('list_objects_v2')
-pages = paginator.paginate(Bucket=BUCKET, Prefix=REGPATH)
-with psycopg.connect(POSTGRESURI) as dbc:
-    with dbc.cursor() as cur:
-        for objects in pages:
-            for info in objects.get('Contents', []):
-                #print(f"checking {info.get('Key')}")
-                if not check_object_in_db(cur, os.path.basename(info.get('Key'))):
-                    print(f"poluting content {info.get('Key')} found in Storage but not in DB")   
+    if options.debug:   logger.setLevel(logging.DEBUG)
+    
+    threads.append(ThreadPoolExecutor().submit(s3thread))    
+    for x in range(0, MAXTHREADS):
+        threads.append(ThreadPoolExecutor().submit(PGConnection))
+
+    while not polution_empty: sleep(0.1)
+    polution.join()
+    logger.info(f"Processed {total} Found {poluted.qsize()} items")
+    while not poluted.empty():
+        logger.info(f"poluting content {poluted.get(timeout=1)} found in Storage but not in DB")
+        poluted.task_done()
